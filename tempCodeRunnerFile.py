ac34de55ea14c61ac3b4b6307387aa5596bc30d1
Xia Zhenchao <xiazhenchao96@gmail.com>
2022-10-31 18:56:48 -0400

    done

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-        if flowPort[i] == tcp.sport and CheckSEQ != tcp.seq:
-            CheckSEQ = tcp.seq
-            ResendStartTime = ts
-        elif flowPort[i] == tcp.sport and CheckSEQ == tcp.seq:
-            ResendEndTime = ts
-            CounterfoRetransmission+=1
-            if ResendEndTime - ResendStartTime <= rto:
-                CounterforDuplicateACK =  CounterforDuplicateACK+1
